7774dab945f8c7aa74aedb911933e49b96662222
Nicolas Gebauer <ngebauer@assertiva.biz>
2026-02-05 18:26:27 -0300

    E3 cqap 3(incompleto)

---
+++ scr/Tree/E3.ts
@@ -0,0 +1,18 @@
+/*
+ * Dada la raíz de un árbol binario, devuelve su profundidad máxima.
+ *
+ * Ejemplo:
+ *  Input:
+ *         4
+ *      2     7
+ *    1   3
+ *  8
+ *
+ *  Output: 4
+ */
+
+import { Node } from "./Ejemplo";
+
+export function maxDepth<T>(root: Node<T> | null): number {
+    return 0;
+}
